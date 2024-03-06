832b373d903180dd673cbe3e631a36241992183a
Quoc Huy Nguyen Dinh <huy.nguyen@sbs.com.au>
2024-03-06 14:06:09 +1100

    Start chrome in disable security mode

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/Applications/Opera.app/Contents/MacOS/Opera --disable-web-security --user-data-dir="/tmp" &
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-web-security --user-data-dir="/tmp" &
 sudo python3 main.py
