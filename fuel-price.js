ce4cf5a9f00176a3a0bad2f4a90d5d1e2706ebd6
Quoc Huy Nguyen Dinh <huy.nguyen@sbs.com.au>
2024-06-23 14:44:03 +1000

    Show fuel results per state, make them clickable to trigger search

--- fuel-price.js
+++ fuel-price.js
@@ -4,15 +4,17 @@ function compare( a, b ) {
   return 0;
 }
 
-fetch('https://fuelprice.io/brands/7-eleven/?fuel_type=ulp98', { cache: "force-cache" })
-  .then((response) => {
-    return response.text();
-  })
-  .then((html) => {
+(async function() {
+  const now = new Date().getTime();
+  let fuelLocations = window.localStorage.getItem('fuel_locations');
+
+  if (!fuelLocations || (now - fuelLocations.timestamp > 600000)) {
+    fuelLocations = {};
+
+    const html = await fetch('https://fuelprice.io/brands/7-eleven/?fuel_type=ulp98', { cache: "force-cache" }).then((response) => { return response.text(); })
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, 'text/html');
     const fuelTypeAgo = doc.querySelectorAll('.fuel-type-ago');
-    const fuelLocation = {};
 
     for(let fi=0; fi<fuelTypeAgo.length; fi+=1) {
       const fuel = fuelTypeAgo[fi];
@@ -25,26 +27,40 @@ fetch('https://fuelprice.io/brands/7-eleven/?fuel_type=ulp98', { cache: "force-c
         const location = anchor.textContent;
         const priceAgo = container.textContent.replace(location, '');
         const state = fuel.closest('ul').previousElementSibling.id.replace('state-', '').replace(/\-/g, ' ');
-        if (!(state in fuelLocation)) {
-          fuelLocation[state] = [];
+        if (!(state in fuelLocations)) {
+          fuelLocations[state] = [];
         }
         const [price, timeAgo] = priceAgo.split(' ');
-        fuelLocation[state].push({state, location, price, timeAgo});
+        fuelLocations[state].push({state, location, price, timeAgo});
       }
     }
 
-    let list = '';
-    for (state in fuelLocation) {
-      fuelLocation[state].sort(compare);
-      list += `${state}\n`;
-      list += fuelLocation[state].splice(0, 7).map((fuel) => {
-        return `$${fuel.price} in ${fuel.location.replace('7-Eleven ', '')} ${fuel.timeAgo}`;
-      }).join('\n');
-      list += `\n\n`;
-    }
-    document.getElementById('fuel-price').value = list;
-  })
-  .catch(function (err) {
-    // There was an error
-    console.warn('Something went wrong.', err);
-  });
+    window.localStorage.setItem('fuel_locations', JSON.stringify({ timestamp: new Date().getTime(), data: fuelLocations }));
+  } else {
+    fuelLocations = JSON.parse(fuelLocations);
+  }
+
+  const container = document.getElementById('fuel-prices');
+
+  for (let state in fuelLocations.data) {
+    fuelLocations.data[state].sort(compare);
+    const stateElement = document.createElement('div');
+    stateElement.innerText = state;
+    stateElement.classList.add('fuel-state');
+    container.appendChild(stateElement);
+
+    fuelLocations.data[state].splice(0, 7).forEach((fuel) => {
+      const fuelLocation = fuel.location.replace('7-Eleven ', '');
+      const fuelElement = document.createElement('button');
+      fuelElement.innerText = `$${fuel.price} in ${fuelLocation} ${fuel.timeAgo}`;
+      fuelElement.value = `${fuelLocation}, ${state}`;
+      fuelElement.addEventListener('click', async (event) => {
+        const results = await provider.search({ query: event.target.value });
+        const location = { location: { x: results[0].x, y: results[0].y } };
+        searchHandler(location);
+        map.flyTo([results[0].y, results[0].x], 15);
+      });
+      container.appendChild(fuelElement);
+    });
+  }
+}());
